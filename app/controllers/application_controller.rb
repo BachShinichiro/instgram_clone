eac3f15b11dbaa0504e346a3cbaded4c12fec84e
BachShinichiroe <kitchen.number@gmail.com>
2020-09-09 07:39:28 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include ApplicationHelper
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = I18n.t(:login_needed)
+      redirect_to new_session_path
+    end
+  end
 end
