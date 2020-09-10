fd671cd223b1d10f6f6340b24edf4690aa738d6b
BachShinichiroe <kitchen.number@gmail.com>
2020-09-10 12:03:56 +0900

    CRUD機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   resources :users, only: [:new, :create, :edit, :update, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
